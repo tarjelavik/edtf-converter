e775016c00c374393728d6a11664be064218bb43
Simon Mathewson <nomistrebla@gmail.com>
2019-01-22 15:09:37 +0100

    Remove peg

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
-(() => {
 
-  const peg = require('pegjs');
-  const fs = require('fs');
-  const pegText = fs.readFileSync('./parser.pegjs', 'utf-8');
-  const parser = peg.generate(pegText);
-
-  module.exports = (input) => {
-    return parser.parse(input);
-  }
-
-})();
+module.exports = function edtfParser() {
+  
+}
